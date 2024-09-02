326dc5d410092390e93cb54c59935044f2a46eb1
laminsurachman <rahman0669@gmail.com>
2024-09-02 16:24:15 +0700

    membuat delete dan update products

--- server/routes/ProductRoutes.js
+++ server/routes/ProductRoutes.js
@@ -52,5 +52,55 @@ router.get("/:name", async (req, res, next) => {
     return res.status(500).json({ error: error.message });
   }
 });
+router.delete("/delete/:id", async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+
+    await product.deleteOne();
+
+    res.status(200).json({ message: "produk berhasil dihapus" });
+  } catch (error) {
+    if (error.name === "CastError") {
+      return res.status(404).json({ message: "Produk tidak ditemukan" });
+    }
+    return res.status(500).json({ message: error.message });
+  }
+});
+router.put(
+  "/update/:id",
+
+  async (req, res) => {
+    try {
+      let product = await Product.findById(req.params.id);
+
+      const { name, desc, category, price, capital, stock, weight } = req.body;
+
+      const profit = price - capital;
+
+      const data = {
+        name: name,
+        desc: desc,
+        category: category,
+        price: price,
+        capital: capital,
+        profit: profit,
+        stock: stock,
+        weight: weight,
+      };
+      product = await Product.findByIdAndUpdate(req.params.id, data, {
+        new: true,
+        runValidators: true,
+      });
+
+      res.status(200).json({ message: "Produk berhasil diupdate", product });
+    } catch (error) {
+      console.log(error);
+      if (error.name === "CastError") {
+        return res.status(404).json({ message: "Produk tidak ditemukan" });
+      }
+      return res.status(500).json({ message: error.message });
+    }
+  }
+);
 
 export default router;
